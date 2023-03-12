56fad533c76b4b3d6f7d1411d7888eec29a12cdc
LeuVigile <122127783+LeuVigile@users.noreply.github.com>
2023-03-12 11:22:09 +0100

    a

--- java.js
+++ java.js
@@ -1,10 +1,36 @@
-function afficher(){
-    // Charger le fichier JSON contenant les données des joueurs
-    fetch('donnees-joueurs.json')
+function add_player(){
+    // Récupérer le formulaire et le bouton de validation
+    const formulaire = document.querySelector('#formulaire-joueur');
+    const boutonValider = document.querySelector('#valider');
+
+    // Ajouter un événement de clic sur le bouton de validation
+    boutonValider.addEventListener('click', (e) => {
+    e.preventDefault(); // Empêcher le comportement par défaut du formulaire
+
+    // Récupérer les valeurs des champs de saisie
+    const nom = document.querySelector('#nom').value;
+    const id = document.querySelector('#id').value;
+    const missions = document.querySelector('#missions').value;
+
+    // Créer un nouvel objet joueur avec les valeurs saisies
+    const nouveauJoueur = {
+        nom: nom,
+        id: id,
+        missions: missions
+    };
+
+    // Envoyer les données JSON au serveur en utilisant fetch()
+    fetch('data.json', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(nouveauJoueur)
+    })
     .then(response => response.json())
     .then(data => {
-        // Boucler sur chaque joueur et afficher les données
-        data.forEach(joueur => {
-        console.log(`Nom: ${joueur.nom}, ID: ${joueur.id}, Missions: ${joueur.missions}`);
-        });
+        console.log('Nouveau joueur ajouté avec succès !');
+        console.log(data); // Afficher les données du fichier JSON mis à jour
+    })
+    .catch(error => console.error(error));
     });}
