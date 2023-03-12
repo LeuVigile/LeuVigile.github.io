677600b4fd6a512f91c9a040ae9c1bc3d012a352
LeuVigile <122127783+LeuVigile@users.noreply.github.com>
2023-03-12 15:03:11 +0100

    a

--- java.js
+++ java.js
@@ -1,4 +1,16 @@
 function add_player() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyBI-ngVWNuy21i2cEphbend_lW8Plb2Ny8",
+        authDomain: "rpima3-brm5.firebaseapp.com",
+        databaseURL: "https://rpima3-brm5-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "rpima3-brm5",
+        storageBucket: "rpima3-brm5.appspot.com",
+        messagingSenderId: "650078138602",
+        appId: "1:650078138602:web:f945f00509e40f0e682793",
+        measurementId: "G-RR2W6K8XWC"
+    };
+    firebase.initializeApp(firebaseConfig);
+
     //récupérer valeurs du formulaire
     var nom = document.getElementById("nom").value;
     var discord_id = document.getElementById("discord_id").value;
