2834e3ec0262298c4fe1ead89a2b2f0c7ff93853
LeuVigile <122127783+LeuVigile@users.noreply.github.com>
2023-03-12 11:32:25 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,36 +1,34 @@
 function add_player(){
-    // Récupérer le formulaire et le bouton de validation
-    const formulaire = document.querySelector('#formulaire-joueur');
-    const boutonValider = document.querySelector('#valider');
-
-    // Ajouter un événement de clic sur le bouton de validation
-    boutonValider.addEventListener('click', (e) => {
-    e.preventDefault(); // Empêcher le comportement par défaut du formulaire
-
-    // Récupérer les valeurs des champs de saisie
-    const nom = document.querySelector('#nom').value;
-    const id = document.querySelector('#id').value;
-    const missions = document.querySelector('#missions').value;
-
-    // Créer un nouvel objet joueur avec les valeurs saisies
-    const nouveauJoueur = {
-        nom: nom,
-        id: id,
-        missions: missions
-    };
-
-    // Envoyer les données JSON au serveur en utilisant fetch()
-    fetch('data.json', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(nouveauJoueur)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Nouveau joueur ajouté avec succès !');
-        console.log(data); // Afficher les données du fichier JSON mis à jour
-    })
-    .catch(error => console.error(error));
-    });}
+      const nom = document.getElementById("nom").value;
+      const id = document.getElementById("id").value;
+      const missions = document.getElementById("missions").value;
+    
+      // Récupère les données du fichier data.json
+      fetch("data.json")
+        .then(response => response.json())
+        .then(data => {
+          // Ajoute un nouvel objet joueur aux données existantes
+          data.joueurs.push({
+            nom: nom,
+            id: id,
+            missions: missions
+          });
+    
+          // Envoie les données mises à jour vers le serveur
+          fetch("data.json", {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+          })
+            .then(response => {
+              console.log("Données mises à jour avec succès !");
+            })
+            .catch(error => {
+              console.error("Erreur lors de la mise à jour des données : ", error);
+            });
+        })
+        .catch(error => {
+          console.error("Erreur lors de la récupération des données : ", error);
+        });}
